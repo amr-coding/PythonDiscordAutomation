7782a7fe4dfe036bff8e0c6c779b7dc5a447e887
unknown <liveworsei@gmail.com>
2020-08-10 11:54:41 +0200

    first commit

--- Chrome.py
+++ Chrome.py
@@ -9,41 +9,24 @@ import time
 
 
 
-
-
-
-class DiscordBot:
-
-
-
-
-        def __init__(self, username, password):
             op = webdriver.ChromeOptions()
             op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             op.add_argument("--headless")
             op.add_argument("--no-sandbox")
             op.add_argument("--disable-dev-sh-usage")
-            self.username = username
-            self.password = password
-            self.driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
-
-
-
-
-
-        def login(self):
-            driver = self.driver
+            driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+            
             driver.get("https://discord.com/login")
             time.sleep(4)
             email = driver.find_element_by_name("email")
             password = driver.find_element_by_name("password")
             email.clear()
             password.clear()
-            email.send_keys(self.username)
-            password.send_keys(self.password)
+            email.send_keys("amr2020ahmed@hotmail.com")
+            password.send_keys("Amrlovemero11@@")
             password.send_keys(Keys.RETURN)
             time.sleep(6)
-            driver.get("https://discord.com/channels/476477031564050432/539185260454215680")
+            driver.get("https://discord.com/channels/433414031789916160/433414031789916162")
             time.sleep(15)
             btn = driver.find_element_by_class_name("markup-2BOw-j.slateTextArea-1Mkdgw.fontSize16Padding-3Wk7zP")
             time.sleep(5)
@@ -63,8 +46,3 @@ class DiscordBot:
                     ,":", timenw.tm_min,
                     ":", timenw.tm_sec )
 
-
-while True:
-    live = DiscordBot("amr2020ahmed@hotmail.com","Amrlovemero11@@")
-    live.login()
-    time.sleep(300)
